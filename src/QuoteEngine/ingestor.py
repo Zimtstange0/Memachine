b5fe8e1b3cd07c55241e949d28c00252fcb5a40a
Kraemer Simon (XC-DA/ESL-S3) <Simon.Kraemer@de.bosch.com>
2022-06-30 13:40:53 +0200

    Ingestor class added.

---
+++ src/QuoteEngine/ingestor.py
@@ -0,0 +1,42 @@
+
+# Quotes importieren (muss noch erstellt werden)
+
+class Ingestor():
+    ingestors = [txt_ingestor]
+    
+    @classmethod
+    def parse(cls, path: str) -> List[Quotes]:
+        for ingestor in cls.ingestors:
+            if ingestor.can_ingest(path):  
+                return ingestor.parse(path)
+
+
+# Imperator soll:
+    # - In: Path
+    # - Out: List with Quotes
+    # data: Alle Ingestoren 
+    # @Classmethod: Checken welcher Ingestor
+    # - Den richtigen Ingestor "aufrufen"
+    # - Eine Liste mit Quotes zurückgeben.
+
+
+# Ingestor(ABC) - Architektur eines Ingestors
+    # - In: -
+    # - Out: -
+    # variable: Fileending
+    # method: check fileending
+        # IN: Path
+        # OUT: TRUE / FALSE
+    
+    # @abstractmethod: parse
+        # pass
+        # return   
+     
+
+# Ingestor
+    # - In: string
+    # - Out: List
+    # data: fileending 
+    
+    # IN: Path
+    # OUT: List with Quotes
